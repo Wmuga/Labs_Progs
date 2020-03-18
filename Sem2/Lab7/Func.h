b2064e5e162b0ade466f4efe5a567a3a8452723f
Wmuga <super.ganga1@yandex.ru>
2020-03-18 12:43:01 +0300

    1

--- Sem2/Lab7/Func.h
+++ Sem2/Lab7/Func.h
@@ -1,8 +1,67 @@
-//
-// Created by Alex on 16.03.2020.
-//
+#include <cstdlib>
 
-#ifndef LAB7_FUNC_H
-#define LAB7_FUNC_H
+struct Cat
+{
+    int type;
+    char*    name;
+    char*    color;
+    int      age;
+    char*    OSName;
+    int      place;
+};
 
-#endif //LAB7_FUNC_H
+void CreateNew(char* in, char* out)
+{
+    FILE* inp  = fopen(in,"r");
+    FILE* outp = fopen(out,"wb");
+    int count = 0;
+    Cat newCat{};
+    while (true)
+    {
+        char buffer[3];
+        fscanf(inp,"%s",&buffer);  newCat.type = atoi(buffer);
+        if (newCat.type==0 || feof(inp)) break;
+        fscanf(inp,"%s",&newCat.name);
+        fscanf(inp,"%s",&newCat.color);
+        fscanf(inp,"%s",&buffer); newCat.age = atoi(buffer);
+        fscanf(inp,"%s",&newCat.OSName);
+        fscanf(inp,"%s",&buffer); newCat.place = atoi(buffer);
+        fwrite(&newCat, sizeof(Cat),1,outp);
+        count++;
+    }
+    fclose(inp);
+    fclose(outp);
+    if (count==0) printf("Created empty file\n");
+    else printf("Created file with %d records\n",count);
+    printf("-------------------------------\nPress any key to continue");
+    fgetc(stdin); fflush(stdin);
+}
+
+void ViewBinary(char* file)
+{
+    char* catType[] = {(char*)"Siam",(char*)"Test"};
+    FILE* pFBin = fopen(file,"rb");
+    int count=0;
+    while (true)
+    {
+        Cat newCat;
+        fread(&newCat, sizeof(newCat),1,pFBin);
+        if (feof(pFBin)) break;
+        printf("Cat N %d:\n  Name:%s\n  Type:%s\n  Age:%d\n  Owner name:%s\n  Place:%d\n",
+                count+1,newCat.name,catType[newCat.type],newCat.age,newCat.OSName,newCat.place);
+        count++;
+    }
+    if (count==0) printf("EmptyFile\n");
+    printf("-------------------------------\nPress any key to continue");
+    fgetc(stdin); fflush(stdin);
+}
+
+void SearchContent(char* file)
+{
+
+}
+
+void ChangeContent(char* file)
+{
+
+}
--- Sem2/Lab7/main.cpp
+++ Sem2/Lab7/main.cpp
@@ -1,7 +1,44 @@
 #include <cstdio>
 #include "Func.h"
+#include <cctype>
 
-int main() {
-
+int main(int argc, char** argv) {
+    printf("Lab 7. Binary File\n");
+    bool end=false;
+    while (!end)
+    {
+        printf("Choose what you want to do:\n"
+               "\'N\' to create new binary file\n"
+               "\'V\' to view content of binary file\n"
+               "\'F\' to find element by key\n"
+               "\'C\' to correct elements\n"
+               "\'X\' to exit\n");
+        char act;
+        act=(char)getc(stdin);
+        fflush(stdin);
+        act =(char)tolower(act);
+        switch(act)
+        {
+            case 'n':
+                CreateNew(argv[1], argv[2]);
+                break;
+            case 'v':
+                ViewBinary(argv[2]);
+                break;
+            case 'f':
+                SearchContent(argv[2]);
+                break;
+            case 'c':
+                ChangeContent(argv[2]);
+                break;
+            case 'x':
+                end=true;
+                break;
+            default:
+                printf("Unknown command\n");
+                break;
+        }
+        printf("-------------------------------");
+    }
     return 0;
 }
