fed1e8bbb013789792ef21bfacf68d99e80039b4
Wmuga <super.ganga1@yandex.ru>
2020-04-14 22:03:42 +0300

    Lab4 Comp
    MB

--- Sem2/Lab4/main.cpp
+++ Sem2/Lab4/main.cpp
@@ -1,28 +1,39 @@
-#include <iostream>
+#include <cstdio>
 #include <cmath>
+#include <cstdlib>
 
-bool RCheckElems(double *a,int start, int end)
+bool RCheckElems(double *array, int start, int end) //Рекурсионная функция
 {
     bool  res = false;
     if (start>end) start=end;
     if (start==end) {
-        res = pow((int)a[start],2)/(end+1)>2;
+        res = pow((int)array[start], 2) / (end + 1) > 2;
         if (res) {
-            std::cout << start + 1 << " element is cool\n";
+            printf("Найден элемент (#%d), удовлетворяющитй условию поиска\n",start+1);
         }else{}
         }
     else
     {
         double oot = (end-start)/3.0;
-        res = RCheckElems(a,start,(trunc(oot))+start);
-        if (!res) res = RCheckElems(a,ceil(oot)+start,trunc(oot*2)+start);
-        if (!res) res = RCheckElems(a,ceil(oot*2)+start,end);
+        res = RCheckElems(array, start, (trunc(oot)) + start);
+        if (!res) res = RCheckElems(array, ceil(oot) + start, end);
     }
     return res;
 }
 
 int main() {
-    double* a = new double[9]; a[0]=0.9; a[1]=0.1; a[2]=1.4; a[3]=1.9; a[4]=1.0; a[5]=0.1; a[6]=3.2; a[7]=0.1; a[8]=2;
-    if(!RCheckElems(a,0,8)) std::cout<<"Welp";
+    int N;
+    system("chcp 65001");
+    printf("Лабораторная работа #4\n");
+    printf("Введите количество элементов массива N=");
+    scanf("%d",&N);
+    fflush(stdin);
+    double *array = new double[N];
+    for (int i=0;i<N;i++) scanf("%lf",&array[i]);
+    fflush(stdin);
+    if (!RCheckElems(array,0,N)) printf("Не найдено элемнтов, удовлетворяющих условию\n");
+    delete []array;
+    printf("Press ENTER");
+    getc(stdin);
     return 0;
 }
