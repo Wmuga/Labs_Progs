6b425f6442ad12c3d510ec26a10b70df99a11bef
Wmuga <super.ganga1@yandex.ru>
2020-04-29 10:29:15 +0300

    Little impovement Lab4

--- Sem2/Lab4/main.cpp
+++ Sem2/Lab4/main.cpp
@@ -2,22 +2,20 @@
 #include <cmath>
 #include <cstdlib>
 
-bool RCheckElems(double *array, int start, int end) //Рекурсия (передаем массив, начало и конец)
+int RCheckElems(double *array, int start, int end) //Рекурсия (передаем массив, начало и конец)
 {
-    bool res; //Наш флаг на условие
     if (start==end) { //Достигли одного элемента
-        res = pow((int)array[start], 2) / (end + 1) > 2; //Проверк условия
-        if (res) {
-            printf("Найден элемент (#%d), удовлетворяющий условию поиска\n",start+1);
-        }else{}
+        printf("%d\n",start);
+        if ((pow((int)array[start], 2)/(end + 1)) > 2) //Проверк условия
+        {
+            printf("Найден элемент (#%d), удовлетворяющий условию\n", start+1);
+            return 1;
+        } else return 0;
         }
     else
-    {
-        int oot = trunc((end-start)/3.0)+start; //отделяем треть
-        //Проверка отделенных частей (start - oot) первая треть (oot+1 - end) оставшиеся 2 трети
-        res = RCheckElems(array, start, oot) or RCheckElems(array, oot+1, end);
+    { //Отделяется одна треть и проверяется по отдельности 1/3 и 2/3
+        return (RCheckElems(array, start,  start + (end-start)/3)) or (RCheckElems(array,  start + (end-start)/3+1, end));
     }
-    return res;
 }
 
 int main() {
@@ -30,7 +28,11 @@ int main() {
     double *array = new double[N];
     for (int i=0;i<N;i++) scanf("%lf",&array[i]);
     fflush(stdin);
-    if (N!=0) if (!RCheckElems(array, 0, N)) printf("Не найдено элементов, удовлетворяющих условию\n"); else{}
+    int res = RCheckElems(array, 0, N);
+    if (N!=0) {
+        if (!res) printf("Не найдено элементов, удовлетворяющих условию\n");
+        else {}
+    }
     else printf("Пустой массив\n");
     delete[]array;
     printf("Press ENTER");
