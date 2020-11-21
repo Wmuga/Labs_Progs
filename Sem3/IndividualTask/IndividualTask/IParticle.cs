42e3c19fd9986c383da7d59544a92e3ecfcb234e
Wmuga <CherkasovAA@mpei.ru>
2020-11-21 20:56:37 +0300

    t
    t

--- Sem3/IndividualTask/IndividualTask/IParticle.cs
+++ Sem3/IndividualTask/IndividualTask/IParticle.cs
@@ -1,6 +1,8 @@
+using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using Projection;
 
 namespace IndividualTask
 {
@@ -20,11 +22,12 @@ namespace IndividualTask
             SetDefaults();
         }
 
-        public IParticle(int x, int y)
+        public IParticle(int x, int y,int z)
         {
             InitializeComponent();
             SetDefaults();
-            this.Location = new Point(x,y);
+            basePoint = new TPoint(x,y,z);
+            this.Location = TTT.Projection(basePoint);
         }
 
         private void SetDefaults()
@@ -32,17 +35,36 @@ namespace IndividualTask
             this.Width = 1;
             this.Height = 1;
             this.BackColor = Color.White;
+            this.baseRadSpeed = 0.01745;
+            this.angle = 0;
         }
         
 
-        public virtual void Tick()
+        public virtual void Tick(double tCoeff)
         {
-            
+            if (angle < 2 * Math.PI) angle += baseRadSpeed * tCoeff;
+            else angle = 0;
+            Location = TTT.Projection(basePoint + TTT.RotateZ(radius,angle));
         }
+/*
+        protected void CheckCoordinates()
+        {
+            const int w = 802;
+            const int h = 367;
+            var x = Location.X;
+            var y =  Location.Y;
+            if  (Location.X > w) x = w * 2 - Location.X;
+            if (Location.Y > h) y = h * 2 - Location.Y;
+            Location = new Point(x,y);
+        }*/
     }
 
     public class Atom : IParticle
     {
+        private void Draw(PaintEventArgs e)
+        {
+            
+        }
         
     }
 
